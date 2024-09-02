d0d1eb81d82c91fc2b124f76ecbe9c1e95906ae3
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-02 19:18:43 +0100

    add voice clone

--- tts/urls.py
+++ tts/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("convert_text", views.convert_text, name="convert_text")
+    path("convert_text", views.convert_text, name="convert_text"),
+    path("voice_clone", views.voice_clone, name="voice_clone"),
+    path("handle_voice", views.handle_voice, name="handle_voice"),
 ]
--- tts/views.py
+++ tts/views.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 from django.views.decorators.http import require_http_methods
 from gtts import gTTS
 import os
+from django.core.files.uploadedfile import UploadedFile
 
 
 # Create your views here.
@@ -15,6 +16,60 @@ class TextToSpeechForm(forms.Form):
                            widget=forms.Textarea(attrs={'class': 'form-control my-3 border', 'id': 'text'}), 
                            required=True,
                            )
+
+class VoiceCloneForm(forms.Form):
+    voice = forms.FileField(label='Upload Your Audio File',
+                                  required=True,
+                                  widget=forms.FileInput(attrs={'class': 'd-none', 'id': 'voice',"accept": "audio/*"}),
+                                  )
+    diffusion = forms.FloatField(label='Diffusion', 
+                                 required=True,
+                                 widget=forms.NumberInput(attrs={'type': 'range',
+                                                                 'id': 'diffusion',
+                                                                 'min': '0',
+                                                                 'max': '20',
+                                                                 'step': '1',
+                                                                 'value': '0',
+                                                                 'oninput': 'changeInput(event)'}),
+                                
+                                 )
+
+    embedding_scale = forms.FloatField(label='Embedding Scale', 
+                                        required=True,
+                                        widget=forms.NumberInput(attrs={'type': 'range',
+                                                                        'id': 'embedding_scale',
+                                                                        'min': '0',
+                                                                        'max': '10',
+                                                                        'step': '1',
+                                                                        'value': '0',
+                                                                        'oninput': 'changeInput(event)'}),
+                                        
+                                        )
+    
+    alpha = forms.FloatField(label='Alpha', 
+                            required=True,
+                            widget=forms.NumberInput(attrs={'type': 'range',
+                                                            'id': 'embedding_scale',
+                                                            'min': '0',
+                                                            'max': '10',
+                                                            'step': '1',
+                                                            'value': '0',
+                                                            'oninput': 'changeInput(event, 0.1, true)'}),
+                            
+                            )
+    
+    beta = forms.FloatField(label='Beta', 
+                            required=True,
+                            widget=forms.NumberInput(attrs={'type': 'range',
+                                                            'id': 'embedding_scale',
+                                                            'min': '0',
+                                                            'max': '10',
+                                                            'step': '1',
+                                                            'value': '0',
+                                                            'oninput': 'changeInput(event, 0.1, true)'}),
+                            
+                            )
+
 
 def index(request):
     # Show the Form
@@ -33,4 +88,24 @@ def convert_text(request):
     filename = "{}.mp3".format(uuid4())   
     speech.save("tts/static/{}".format(filename))
     return HttpResponse(f'<audio controls><source src="/static/{filename}" type="audio/mpeg"></audio>')
+
+
+def voice_clone(request):
+    return render(request, 'tts/voice_clone.html', {'form': VoiceCloneForm()})
+
+@require_http_methods(["POST"])
+def handle_voice(request):
+
+    form = VoiceCloneForm(request.POST, request.FILES)
+    if not form.is_valid():
+        return HttpResponse(f'<p style="color:red;">{form.errors}</p>')
+    # save the uploaded file
+    voice: UploadedFile = form.cleaned_data["audio"]
+    diffusion = form.cleaned_data["diffusion"]
+    embedding_scale = form.cleaned_data["embedding_scale"]
+    alpha = form.cleaned_data["alpha"]
+    beta = form.cleaned_data["beta"]
+
     
+
+    return HttpResponse(request.POST)
