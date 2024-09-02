c44f986e90bab48045bd472049799849fb283d85
John David <davemcsavvii@gmail.com>
2024-09-02 23:59:20 +0000

    feat: Update voice cloning view to support multiple texts and generate audio files

--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,14 @@ def get_media_path(filename: str, folder: str | None = None) -> str:
     )
 
 
+def get_media_url(filename: str, folder: str | None = None) -> str:
+    return (
+        f"{settings.MEDIA_URL}{folder}/{filename}"
+        if folder
+        else f"{settings.MEDIA_URL}{filename}"
+    )
+
+
 def get_voice_path(filename: str, folder: str | None = None) -> str:
     return (
         os.path.join(settings.VOICE_ROOT, folder, filename)
--- voice_cloning/views.py
+++ voice_cloning/views.py
@@ -1,9 +1,31 @@
+import datetime
+import os
+
+import numpy as np
 from django import forms
+from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpResponse
 from django.views.generic import FormView
+from scipy.io.wavfile import write
+
+from app import utils
+from StyleTTS2 import msinference
+from StyleTTS2.text_utils import split_and_recombine_text
 
 
 class VoiceCloneForm(forms.Form):
+    text = forms.CharField(
+        label="Enter Your Text",
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control my-3 border",
+                "id": "text",
+                "rows": "4",
+                "placeholder": "Enter your text here",
+            }
+        ),
+        required=True,
+    )
     voice = forms.FileField(
         label="Upload Your Audio File",
         required=True,
@@ -11,7 +33,7 @@ class VoiceCloneForm(forms.Form):
             attrs={"class": "d-none", "id": "voice", "accept": "audio/*"}
         ),
     )
-    diffusion = forms.FloatField(
+    diffusion = forms.IntegerField(
         label="Diffusion",
         required=True,
         widget=forms.NumberInput(
@@ -27,7 +49,7 @@ class VoiceCloneForm(forms.Form):
         ),
     )
 
-    embedding_scale = forms.FloatField(
+    embedding_scale = forms.IntegerField(
         label="Embedding Scale",
         required=True,
         widget=forms.NumberInput(
@@ -81,13 +103,43 @@ class VoiceCloneView(FormView):
     form_class = VoiceCloneForm
 
     def form_valid(self, form):
-        voice = form.cleaned_data["voice"]
+        text = form.cleaned_data["text"]
+        voice: UploadedFile = form.cleaned_data["voice"]
         diffusion = form.cleaned_data["diffusion"]
         embedding_scale = form.cleaned_data["embedding_scale"]
         alpha = form.cleaned_data["alpha"]
         beta = form.cleaned_data["beta"]
 
-        return HttpResponse(self.request.POST)
+        texts = split_and_recombine_text(text)
+        id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{id}.wav"
+        if voice.name:
+            filename = f"{os.path.splitext(voice.name)[0]}-{id}.wav"
+        voice_path = utils.get_voice_path(filename, "uploads")
+        os.makedirs(os.path.dirname(voice_path), exist_ok=True)
+        with open(voice_path, "wb") as f:
+            for chunk in voice.chunks():
+                f.write(chunk)
+        voice_tensor = msinference.compute_style(voice_path)
+        audios = []
+        for text in texts:
+            audios.append(
+                msinference.inference(
+                    text,
+                    voice_tensor,
+                    alpha=alpha,
+                    beta=beta,
+                    diffusion_steps=diffusion,
+                    embedding_scale=embedding_scale,
+                )
+            )
+        upload_path = utils.get_media_path(filename, "clones")
+        file_url = utils.get_media_url(filename, "clones")
+        os.makedirs(os.path.dirname(upload_path), exist_ok=True)
+        write(upload_path, 24000, np.concatenate(audios))
+        return HttpResponse(
+            f'<audio controls><source src="{file_url}" type="audio/wav"></audio>'
+        )
 
     def form_invalid(self, form):
         return HttpResponse(f'<p style="color:red;">{form.errors}</p>')
