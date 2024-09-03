d576a042f0203a0d1ce8d4ba13fcffc3f70f70ed
John David <davemcsavvii@gmail.com>
2024-09-03 20:57:53 +0000

    chore: Update run.py to set DJANGO_SETTINGS_MODULE dynamically

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 # start the django server
+import os
 import webbrowser
 
 from django.core.management import execute_from_command_line
+
+from app.env import env
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"app.settings.{env}")
 
 webbrowser.open("http://localhost:8000", new=2)
 execute_from_command_line(["manage.py", "runserver", "localhost:8000"])
